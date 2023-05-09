499f6f4942b4a0932538063e3a640603166ec826
viniciusgrecco <vgrecco23@icloud.com>
2023-05-08 23:27:43 -0300

    conceitoB

---
+++ ex5-afundados.py
@@ -0,0 +1,80 @@
+def afundados(embarcacoes,tabuleiro):
+    contagem = 0
+    for navio in embarcacoes:
+        if navio == 'porta-aviões':
+            lista_navios = embarcacoes[navio]
+            for navio in lista_navios:
+                 x = navio[0][0]
+                 y = navio[0][1]
+                 x1= navio[1][0]
+                 y1= navio[1][1]
+                 x2=navio[2][0]
+                 y2=navio[2][1]
+                 x3=navio[3][0]
+                 y3=navio[3][1] 
+                 coordenada1 = tabuleiro[x][y]
+                 coordenada2 = tabuleiro[x1][y1]
+                 coordenada3 = tabuleiro[x2][y2]
+                 coordenada4 = tabuleiro[x3][y3]
+                 if coordenada1 == 'X' and coordenada2 == 'X' and coordenada3 == 'X' and coordenada4 == 'X':
+                    contagem += 1
+
+        if navio == 'navio-tanque':
+             lista_navios = embarcacoes[navio]
+             for navio in lista_navios:
+                 x = navio[0][0]
+                 y = navio[0][1]
+                 x1= navio[1][0]
+                 y1= navio[1][1]
+                 x2=navio[2][0]
+                 y2=navio[2][1]
+                 coordenada1 = tabuleiro[x][y]
+                 coordenada2 = tabuleiro[x1][y1]
+                 coordenada3 = tabuleiro[x2][y2]
+                 if coordenada1 == 'X' and coordenada2 == 'X' and coordenada3 == 'X':
+                    contagem += 1
+
+        if navio == 'contratorpedeiro':
+             lista_navios = embarcacoes[navio]
+             for navio in lista_navios:
+                x = navio [0][0]
+                y = navio [0][1]
+                x1 = navio [1][0]
+                y1 = navio [1][1]
+                coordenada1 = tabuleiro[x][y]
+                coordenada2 = tabuleiro[x1][y1]
+                if coordenada1 == 'X' and coordenada2 == 'X':
+                     contagem += 1
+
+        if navio == 'submarino':
+             lista_navios = embarcacoes[navio]
+             for navio in lista_navios:
+                 for posicao in navio:
+                     x = posicao[0]
+                     y = posicao[1] 
+                     coordenada1 = tabuleiro[x][y]
+                     if coordenada1 == 'X':
+                        contagem += 1
+    return contagem
+
+frota =  {'contratorpedeiro': [[[7, 6], [8, 6]], [[5, 3], [5, 4]], [[6, 8], [6, 9]]],
+'navio-tanque': [[[4, 2], [4, 3], [4, 4]], [[6, 2], [7, 2], [8, 2]]],
+'porta-aviões': [[[0, 5], [0, 6], [0, 7], [0, 8]]],
+'submarino': [[[1, 4]], [[9, 8]], [[5, 6]], [[8, 4]]]}
+
+tabuleiro = [
+
+[0, '-', 0, 0, 0, 1, 1, 1, 1, '-'],
+['-', 0, 0, '-', 'X', 0, '-', '-', 0, '-'],
+[0, 0, 0, '-', '-', '-', '-', 0, '-', '-'],
+[0, '-', '-', 0, '-', '-', '-', 0, 0, 0],
+['-', '-', 1, 1, 1, '-', '-', '-', 0, 0],
+[0, 0, '-', 'X', 'X', 0, 'X', '-', '-', 0],
+[0, '-', 1, '-', '-', '-', '-', '-', 1, 'X'],
+[0, 0, 1, 0, 0, '-', 1, '-', 0, 0],
+[0, '-', 'X', 0, 1, 0, 'X', '-', '-', '-'],
+[0, 0, '-', '-', 0, 0, '-', '-', 'X', 0]
+]
+
+resultado = afundados(frota, tabuleiro)
+print(resultado)
